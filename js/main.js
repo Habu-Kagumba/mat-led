306559d934705bc7561f2b3a3cc3f599107b22f0
Herbert Kagumba <habukagumba@gmail.com>
2017-09-23 13:54:22 +0300

    Clean up

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 var tag = document.createElement('script');
-	tag.src = "https://www.youtube.com/iframe_api";
-		var firstScriptTag = document.getElementsByTagName('script')[0];
-			firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+tag.src = "https://www.youtube.com/iframe_api";
 
-			var player;
-			function onYouTubeIframeAPIReady() {
-						player = new YT.Player('player', {
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player('player', {
 							   height: '100',
 							   width: '150',
 							   videoId: 'ZDHzzVXXl70',
@@ -19,6 +21,7 @@ var tag = document.createElement('script');
 										}
 							});
 			}
+
 function onPlayerReady(event) {
 			event.target.playVideo();
 }
