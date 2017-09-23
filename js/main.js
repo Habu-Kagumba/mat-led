37e01c679fdbc6823b1ba8ce60c04e8ef3d00ec4
Herbert Kagumba <habukagumba@gmail.com>
2017-09-23 13:55:58 +0300

    Clean up

--- js/main.js
+++ js/main.js
@@ -8,20 +8,20 @@ var player;
 
 function onYouTubeIframeAPIReady() {
 	player = new YT.Player('player', {
-							   height: '100',
-							   width: '150',
-							   videoId: 'ZDHzzVXXl70',
-							   playerVars: {
-							   		controls: 0,
-							   		loop: 1,
-							   		playlist: 'PLT8TXiPIBAQqb8SyD5Rcwr8cc9neJavNf'
-							   },
-							   	events: {
-									'onReady': onPlayerReady,
-										}
-							});
-			}
+		height: '100',
+		width: '150',
+		videoId: 'ZDHzzVXXl70',
+		playerVars: {
+			controls: 0,
+			loop: 1,
+			playlist: 'PLT8TXiPIBAQqb8SyD5Rcwr8cc9neJavNf'
+		},
+		events: {
+			'onReady': onPlayerReady,
+		}
+	});
+}
 
 function onPlayerReady(event) {
-			event.target.playVideo();
+	event.target.playVideo();
 }
